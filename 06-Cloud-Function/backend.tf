d559a4cb7fd14a60bb6cb6d3a7ed21fe1987fcfc
Cedric K <ck@home.lab>
2026-01-08 00:10:45 +0200

    Deploy 06-Cloud-Function infrastructure with image upload bucket

--- 06-Cloud-Function/backend.tf
+++ 06-Cloud-Function/backend.tf
@@ -16,7 +16,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "" # GCS bucket for Terraform Remote State
+    bucket = "cft-lab-state-ck-50d7376d" # GCS bucket for Terraform Remote State
     prefix = "terraform/state/06/"
   }
 }
